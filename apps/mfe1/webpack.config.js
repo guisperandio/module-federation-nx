a3404b5d370ce41fe5581283040e4b0ded4083a8
Guilherme Santos <guilherme.santos@sap.com>
2020-11-27 16:56:03 +0000

    webpack config

--- apps/mfe1/webpack.config.js
+++ apps/mfe1/webpack.config.js
@@ -1,26 +1,25 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   output: {
-    publicPath: "http://localhost:3000/",
-    uniqueName: "mfe1"
+    publicPath: 'http://localhost:3000/',
+    uniqueName: 'mfe1',
   },
   optimization: {
     // Only needed to bypass a temporary bug
-    runtimeChunk: false
+    runtimeChunk: false,
   },
   plugins: [
     new ModuleFederationPlugin({
-      
-        // For remotes (please adjust)
-        name: "mfe1",
-        library: { type: "var", name: "mfe1" },
-        filename: "remoteEntry.js",
-        exposes: {
-            './Module': './apps/mfe1/src/app/flights/flights.module.ts',
-        },        
+      // For remotes (please adjust)
+      name: 'mfe1',
+      library: { type: 'var', name: 'mfe1' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Module': './apps/mfe1/src/app/flights/flights.module.ts',
+      },
 
-        shared: ["@angular/core", "@angular/common", "@angular/router"]
-    })
+      shared: ['@angular/core', '@angular/common', '@angular/router'],
+    }),
   ],
 };
